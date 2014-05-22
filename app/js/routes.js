7e1d45bf624f85a6fbe1587581d099c1cb1de324
Syeoryn <syeoryn@gmail.com>
2014-05-21 22:32:24 -0700

    Fixes router links for deployment architecture.

--- app/js/routes.js
+++ app/js/routes.js
@@ -6,23 +6,23 @@ angular.module('myApp.routes', ['ngRoute'])
    // which should only be available while logged in
    .config(['$routeProvider', function($routeProvider) {
       $routeProvider.when('/home', {
-         templateUrl: 'partials/home.html',
+         templateUrl: 'app/partials/home.html',
          controller: 'HomeCtrl'
       });
 
       $routeProvider.when('/chat', {
-         templateUrl: 'partials/chat.html',
+         templateUrl: 'app/partials/chat.html',
          controller: 'ChatCtrl'
       });
 
       $routeProvider.when('/account', {
          authRequired: true, // must authenticate before viewing this page
-         templateUrl: 'partials/account.html',
+         templateUrl: 'app/partials/account.html',
          controller: 'AccountCtrl'
       });
 
       $routeProvider.when('/login', {
-         templateUrl: 'partials/login.html',
+         templateUrl: 'app/partials/login.html',
          controller: 'LoginCtrl'
       });
 
